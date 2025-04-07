1d80dd7240abaa0530e8860680e26e8e1b2f840b
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2025-04-08 00:01:10 +0530

    added icons to tools

--- src/app/(dashboard)/tools/developer-tech/github-profile-analyzer/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>github profile comparator</div>
-  )
-}
-
-export default page
--- src/app/(dashboard)/tools/developer-tech/json-formatter-validator/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>json formatter and validator</div>
-  )
-}
-
-export default page
