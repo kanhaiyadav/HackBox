c3b3467e260218473c1e7690281eda10869f761b
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2025-05-08 12:18:12 +0530

    minor ui fixes

--- src/app/(dashboard)/home/userSpecificSection.tsx
+++ src/app/(dashboard)/home/userSpecificSection.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import ToolCard from "@/components/toolCard";
-// import Image from "next/image";
+import Image from "next/image";
 import React from "react";
-import { selectAllTools } from "@/lib/features/tools/tools.selector";
-import { useSelector } from "react-redux";
 
 const UserSpecificSection = () => {
     const [active, setActive] = React.useState(0);
-
-    const tools = useSelector(selectAllTools);   
 
     const navs = [
         { name: "Recent Used", id: "recent" },
@@ -19,14 +14,14 @@ const UserSpecificSection = () => {
 
     return (
         <div className="w-full">
-            <div className="flex items-center w-full gap-[1px] border-b-3 border-white/10">
+            <div className="flex items-center w-full gap-4 sm:gap-6 border-b-2 border-accent">
                 {navs.map((nav, index) => (
                     <button
                         key={index}
-                        className={`px-3 md:px-6 py-2 md:py-3 rounded-t-lg md:rounded-t-xl text-xs xss:text-sm md:text-base ${
+                        className={`text-xs xss:text-sm md:text-base py-2 px-1 ${
                             active === index
-                                ? "bg-accent border-2 border-white/5 border-b-0"
-                                : "bg-gray-400/10"
+                                ? "border-b-2 border-primary text-primary font-semibold"
+                                : "border-b-2 border-transparent"
                         }`}
                         onClick={() => setActive(index)}
                     >
@@ -35,17 +30,15 @@ const UserSpecificSection = () => {
                 ))}
             </div>
             <div className="grow  py-4 overflow-x-auto overflow-y-hidden styled-scrollbar">
-                {/* <div className="mb-[-10px] mt-[10px]">
-                    <Image src={'/no-data.png'} alt="no-data" width={60} height={60} className="mx-auto" />
+                <div className="mb-[-10px] mt-[10px]">
+                    <Image
+                        src={"/no-data.png"}
+                        alt="no-data"
+                        width={60}
+                        height={60}
+                        className="mx-auto"
+                    />
                     <p className="w-fit m-auto text-white/40">No data exits!</p>
-                </div> */}
-                <div className="flex gap-4 w-fit">
-                    {tools.slice(23, 35).map((tool, index) => (
-                        <ToolCard key={index} tool={tool} className="w-[120px]"/>
-                    ))}
-                    {/* {tools.map((tool, index) => (
-                        <ToolCard key={index} tool={tool} className="w-[120px]"/>
-                    ))} */}
                 </div>
             </div>
         </div>
--- src/components/dashboardHeader.tsx
+++ src/components/dashboardHeader.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { FaCoffee } from "react-icons/fa";
 import { GrAnnounce } from "react-icons/gr";
-// import { FaGithub } from "react-icons/fa";
-// import { HiStar } from "react-icons/hi2";
-// import { HiOutlineStar } from "react-icons/hi2";
 import { LuSunMedium } from "react-icons/lu";
 import Link from "next/link";
 import { Button } from "./ui/button";
@@ -26,7 +22,7 @@ const ToolNav = async () => {
     console.log(session, "session");
     
     return (
-        <nav className="flex justify-between items-center px-4 py-4 rounded-xl w-full foreground shadow-foreground my-4">
+        <nav className="flex justify-between items-center px-4 py-3 rounded-xl w-full foreground shadow-foreground my-2">
             <SearchBar />
             <div className="w-fit flex items-center gap-4 sm:gap-6">
                 {/* <div className="flex items-center gap-1">
@@ -42,26 +38,6 @@ const ToolNav = async () => {
                         </TooltipTrigger>
                         <TooltipContent>
                             <p>What new</p>
-                        </TooltipContent>
-                    </Tooltip>
-                    {/* <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Link href="/">
-                                <FaGithub className="cursor-pointer text-xl text-white/40 hover:text-primary" />
-                            </Link>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>Star HackBox</p>
-                        </TooltipContent>
-                    </Tooltip> */}
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <Link href="/">
-                                <FaCoffee className="cursor-pointer text-xl text-white/40 hover:text-primary" />
-                            </Link>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>Buy me a Coffee</p>
                         </TooltipContent>
                     </Tooltip>
                     <Tooltip>   
